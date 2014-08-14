d682f7acb3d8ac1e7c101183a554e3af9b4896ec
IWantMore <cisco_guzman@yahoo.com>
2014-08-14 13:23:53 -0500

    Added fortune

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-  res.render('about');
+  res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
